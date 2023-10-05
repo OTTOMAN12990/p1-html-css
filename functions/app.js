e87fc7347181ff7d5c150f3d6e5a7e8369fd6a72
OTTOMAN12990 <9014996@student.zadkine.nl>
2023-10-05 09:58:12 +0200

    opdracht 6.3

---
+++ functions/app.js
@@ -0,0 +1,10 @@
+
+function changeColor(){
+    document.getElementById("title").style.display = "none";
+    document.getElementById("poep").innerText ="gone"
+}
+funcion changeFont(){
+    document.getElementById()
+}
+
+
